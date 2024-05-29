daaa3ac59adc02fb5f3b9db4b158e70205ead3e8
shinyingrid <shinyingrid88@gmail.com>
2024-05-29 14:48:48 +0530

    Jenkins Demo Project first push

---
+++ MavenPractice/src/test/java/ContactPackage/ContactTest.java
@@ -0,0 +1,22 @@
+package ContactPackage;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test
+	public void createContTest()
+	{
+		String USERNAME = System.getProperty("username");
+		System.out.println("createContTest==>1");
+		System.out.println("username is :"+USERNAME);
+	}
+	
+	@Test
+	public void deletContTest()
+	{
+		String PASSWORD = System.getProperty("password");
+		System.out.println("deletContTest==>2");
+		System.out.println("password is :"+PASSWORD);
+	}
+}
---
+++ MavenPractice/src/test/java/OrgPackage/OrgTest.java
@@ -0,0 +1,22 @@
+package OrgPackage;
+
+import org.testng.annotations.Test;
+
+public class OrgTest 
+{
+	@Test
+	public void createOrgTest()
+	{
+		String BROWSER = System.getProperty("browser");
+		System.out.println("createOrgTest==>3");
+		System.out.println("browser is :"+BROWSER);
+	}
+	
+	@Test
+	public void deleteOrgTest()
+	{
+		String URL = System.getProperty("url");
+		System.out.println("deleteOrgTest==>4");
+		System.out.println("url is :"+URL);
+	}
+}
